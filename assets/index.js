02e7ff85cecad0ecc168d835fc49875ca016db77
DarcJC <me@darc.pro>
2020-12-26 02:46:02 +0800

    fix: header text and small text not rendered

--- assets/index.js
+++ assets/index.js
@@ -9,37 +9,49 @@ function parseDate(date_text) {
     return d;
 }
 
-const data = {};
-const a = getQueryVariable("a");
-const _default_load_config = () => {
-    // loading data from url param
-    data["title"] = getQueryVariable("t") ? getQueryVariable("t") : "Darc的倒计时";
-    data["background"] = {
-        blur: getQueryVariable("blur") ? parseInt(getQueryVariable("blur")) : 0,
-        origin_type: getQueryVariable("origin_type") ? getQueryVariable("origin_type") : "picsum",
-        grayscale: getQueryVariable("gray") ? Boolean(getQueryVariable("gray")) : false,
-        img_url: getQueryVariable("img_url") ? getQueryVariable("img_url") : null,
+const _page_data = {};
+
+document.addEventListener('readystatechange', (event) => {
+    if (document.readyState === "complete") {
+        const a = getQueryVariable("a");
+        const _default_load_config = () => {
+            // loading data from url param
+            _page_data["title"] = getQueryVariable("t") ? getQueryVariable("t") : "Darc的倒计时";
+            _page_data["background"] = {
+                blur: getQueryVariable("blur") ? parseInt(getQueryVariable("blur")) : 0,
+                origin_type: getQueryVariable("origin_type") ? getQueryVariable("origin_type") : "picsum",
+                grayscale: getQueryVariable("gray") ? Boolean(getQueryVariable("gray")) : false,
+                img_url: getQueryVariable("img_url") ? getQueryVariable("img_url") : null,
+            }
+            _page_data["header_text"] = getQueryVariable("header_text") ? getQueryVariable("header_text") : "一个奇怪的⏳";
+            _page_data["small_text"] = getQueryVariable("small_text") ? getQueryVariable("small_text") : "猜猜看怎么用?";
+            _page_data["date"] = getQueryVariable("date") ? parseDate(getQueryVariable("date")) : new Date();
+        }
+        if (a && _global_config.static_data[a]) {
+            // loading data from _global_config
+            Object.assign(_page_data, _global_config.static_data[a]);
+            _page_data["date"] = parseDate(_page_data["date"]);
+        } else {
+            _default_load_config();
+        }
+        _page_data["countdown_text"] = "";
+
+        document.getElementById("main_box").innerHTML = template("main_content", _page_data);
+
+        setInterval(() => {
+            let res = Math.round((_page_data["date"] - new Date()) / 1000);
+            _page_data["countdown_text"] = `${Math.round(3600 / 24 / res)} 天 ${Math.round(res / 3600 % 24)} 时 ${Math.round(res / 60 % 60)} 分 ${Math.round(res % 60)} 秒`;
+            document.getElementById("countdown_text").innerText = _page_data["countdown_text"];
+        }, 1000);
+
+        setInterval(() => {
+            document.getElementById("title").innerText = (_page_data["title"]);
+            document.getElementById("header_text").innerText = (_page_data["header_text"]);
+            document.getElementById("small_text").innerText = (_page_data["small_text"]);
+        }, 1000);
+
+
+        getBackgroundImageFactory(_page_data.background).setBackground("background");
     }
-    data["header_text"] = getQueryVariable("header_text") ? getQueryVariable("header_text") : "一个奇怪的⏳";
-    data["small_text"] = getQueryVariable("small_text") ? getQueryVariable("small_text") : "猜猜看怎么用?";
-    data["date"] = getQueryVariable("date") ? parseDate(getQueryVariable("date")) : new Date();
-}
-if (a && _global_config.static_data[a]) {
-    // loading data from _global_config
-    Object.assign(data, _global_config.static_data[a]);
-    data["date"] = parseDate(data["date"]);
-} else {
-    _default_load_config();
-}
-data["countdown_text"] = "";
+});
 
-const html = template("main_content", data);
-document.getElementById("main_box").innerHTML = html;
-
-setInterval(()=>{
-    let res = Math.round((data["date"] - new Date()) / 1000);
-    data["countdown_text"] = `${Math.round(3600 / 24 / res)} 天 ${Math.round(res/3600%24)} 时 ${Math.round(res/60%60)} 分 ${Math.round(res%60)} 秒`;
-    document.getElementById("countdown_text").innerText = data["countdown_text"];
-}, 1000);
-
-getBackgroundImageFactory(data.background).setBackground("background");
